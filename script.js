11f65259903fd15400048ebb174973a8a0ae07ee
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2023-12-02 14:49:51 -0300

    atualizações

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const opcoes=[...document.querySelectorAll(".barra-opc")]
-
+const notic=[...document.querySelectorAll(".inbox-item")]
+const tirarSelecao=()=>{
+    const itensSelecionados=[...document.querySelectorAll(".selecionado")]
+    itensSelecionados.map((el)=>{
+        el.classList.remove("selecionado")
+        el.classList.add("teste")
+    
+    })
+}
+const mudarNoticia=()=>{
+    const itensSelecionados=[...document.querySelectorAll(".noticaSelecionada")]
+    itensSelecionados.map((el)=>{
+        el.classList.remove("noticaSelecionada")
+    })
+}
 opcoes.map((el)=>{
     el.addEventListener("click",(evt)=>{
+        tirarSelecao()
         const el=evt.target
+        el.style.cursor="grab"
+        el.classList.remove("teste")
         el.classList.toggle("selecionado")
+       
+        
     })
 
 })
+
+notic.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        mudarNoticia()
+        el.classList.toggle("noticaSelecionada")
+        let tela=document.getElementById("tela-info")
+        tela.innerHTML="teste"
+    })
+})
+console.log(notic)
